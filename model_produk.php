7e048fe2947fe4682ac260019ffebbf9b31ea1d8
qorina12 <qorinaprameswari12@gmail.com>
2022-09-01 10:04:00 +0700

    test

---
+++ model_produk.php
@@ -0,0 +1,26 @@
+<?php
+    // buat koneksi dengan database mysql
+    function buatKoneksi(){
+        $dbhost = "localhost";
+        $dbuser = "root";
+        $dbpass = "";
+        $dbname = "db_polinema";
+        return mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    }
+
+    // ambil tabel mahasiswa
+    function getTableProduk() {
+        $link = buatKoneksi();
+        $query = "SELECT * FROM produk";
+        $result = mysqli_query($link, $query);
+
+    // ambil semua isi tabel ke dalam bentu array 2 dimensi
+    $hasil = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $hasil;
+    }
+
+    echo "<pre>";
+    print_r(getTableProduk());
+    echo "</pre>";
+
+?>
