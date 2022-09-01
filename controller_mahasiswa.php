2157df33af305384649bf04d8fe5d01c4d6897aa
qorina12 <qorinaprameswari12@gmail.com>
2022-09-01 10:15:41 +0700

    test2

--- controller_mahasiswa.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'model_mahasiswa.php';
-    $isiTabelMahasiswa = getTableMahasiswa();
-    include 'view_mahasiswa.php';
-?>
--- controller_produk.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'model_produk.php';
-    $isiTabelProduk = getTableProduk();
-    include 'view_produk.php';
-?>
